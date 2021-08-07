4b5241e0ea28ed5fb076ae5dece9b14d6ca704a2
Alvaro Padilla <alvaro.padilla@bitsamericas.com>
2021-08-07 15:36:09 -0500

    Agregando directorio principal del proyecto

---
+++ src/main/java/com/springboot/app/springboot/people/app/SpringBootPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.springboot.people.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPeopleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPeopleApplication.class, args);
+    }
+
+}
